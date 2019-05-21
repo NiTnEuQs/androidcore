3b2ea12d079c1b7cda043c19150695358e80246f
doutriaux <qdoutriaux@servicesnomades.com>
2019-05-21 15:24:02 +0200

    Adding multiple FileUtils functions; Commenting FileUtils.java file;

--- app/src/main/java/fr/dtrx/androidcore/utils/FileUtils.java
+++ app/src/main/java/fr/dtrx/androidcore/utils/FileUtils.java
@@ -4,9 +4,14 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Environment;
 
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -16,8 +21,10 @@ import java.util.Locale;
 
 import androidx.core.content.FileProvider;
 
+@SuppressWarnings("unused")
 public class FileUtils {
 
+    @SuppressWarnings("unused")
     public static File createImageFile(Context context) throws IOException {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.FRANCE).format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
@@ -30,18 +37,41 @@ public class FileUtils {
         );
     }
 
-    public static File copyFile(File source, File dest) throws IOException {
+    /**
+     * Copy a file
+     *
+     * @param source Source file
+     * @param dest   Destination file
+     * @return The destination file, null else
+     */
+    @SuppressWarnings("unused")
+    public static File copyFile(File source, File dest) {
         try (InputStream is = new FileInputStream(source); OutputStream os = new FileOutputStream(dest)) {
             byte[] buffer = new byte[1024];
             int length;
             while ((length = is.read(buffer)) > 0) {
                 os.write(buffer, 0, length);
             }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+
+            return null;
         }
 
         return dest;
     }
 
+    /**
+     * Delete a file
+     *
+     * @param context Context
+     * @param file    File to remove
+     */
+    @SuppressWarnings({"unused", "ResultOfMethodCallIgnored"})
     public static void deleteFile(final Context context, final File file) {
         if (file != null && file.exists()) {
             file.delete();
@@ -85,12 +115,80 @@ public class FileUtils {
         }
     }
 
-    public static Uri fileToUri(Context context, File sourceFile) {
+    /**
+     * Get URI from file
+     *
+     * @param context    Context
+     * @param sourceFile The file to read
+     * @return An URI from the file, null else
+     */
+    @SuppressWarnings("unused")
+    public static Uri uriFromFile(Context context, File sourceFile) {
         return FileProvider.getUriForFile(
                 context,
                 context.getPackageName() + ".provider",
                 sourceFile
         );
+    }
+
+    /**
+     * Get object from a raw json file
+     *
+     * @param context       Context
+     * @param rawResourceId Resource id of the raw file
+     * @return An object that represents the Json file content
+     */
+    @SuppressWarnings("unused")
+    public static <T> T jsonFromRaw(Context context, int rawResourceId, Class<T> clazz) {
+        return new Gson().fromJson(stringFromInputStream(context.getResources().openRawResource(rawResourceId)), clazz);
+    }
+
+    /**
+     * Read text from input stream
+     *
+     * @param inputStream The input stream to read
+     * @return A string with the input stream content, null else
+     */
+    @SuppressWarnings({"unused", "ResultOfMethodCallIgnored"})
+    public static String stringFromInputStream(InputStream inputStream) {
+        try {
+            byte[] bytes = new byte[inputStream.available()];
+            inputStream.read(bytes, 0, bytes.length);
+
+            return new String(bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+
+            return null;
+        }
+    }
+
+    /**
+     * Read text from file
+     *
+     * @param file The file to read
+     * @return A string with the file content, null else
+     */
+    @SuppressWarnings("unused")
+    public static String stringFromFile(File file) {
+        try {
+            StringBuilder text = new StringBuilder();
+
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                text.append(line);
+                text.append('\n');
+            }
+            br.close();
+
+            return text.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+
+            return null;
+        }
     }
 
 }
