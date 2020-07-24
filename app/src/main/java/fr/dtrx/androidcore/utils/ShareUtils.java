08486039090875d1a8c1be36f7afeff00f8828b6
doutriaux <qdoutriaux@servicesnomades.com>
2020-07-24 17:29:19 +0200

    Adding shareFile() and shareViews() in ShareUtils

--- app/src/main/java/fr/dtrx/androidcore/utils/ShareUtils.java
+++ app/src/main/java/fr/dtrx/androidcore/utils/ShareUtils.java
@@ -2,6 +2,13 @@ package fr.dtrx.androidcore.utils;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.view.View;
+
+import java.io.File;
+
+import androidx.core.content.FileProvider;
 
 @SuppressWarnings("unused")
 public class ShareUtils {
@@ -13,6 +20,28 @@ public class ShareUtils {
             i.putExtra(Intent.EXTRA_SUBJECT, "Partage");
             i.putExtra(Intent.EXTRA_TEXT, url);
             context.startActivity(Intent.createChooser(i, "Partage"));
+        }
+    }
+
+    public static void shareFile(Context context, File file) {
+        if (file.exists()) {
+            Uri fileUri = FileProvider.getUriForFile(context, context.getApplicationContext().getPackageName() + ".utils.GenericFileProvider", file);
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.putExtra(Intent.EXTRA_STREAM, fileUri);
+            shareIntent.setType("application/vnd.openxmlformats-officedocument.presentationml.presentation");
+            context.startActivity(Intent.createChooser(shareIntent, "Partager un fichier"));
+        }
+    }
+
+    public static void shareViews(Context context, View... views) {
+        Bitmap b = ImageUtils.getBitmapFromViews(context, views);
+
+        if (b != null) {
+            File file = ImageUtils.savePicture(context, b);
+            if (file != null) {
+                DialogUtils.getInstance().dismiss();
+                shareFile(context, file);
+            }
         }
     }
 
